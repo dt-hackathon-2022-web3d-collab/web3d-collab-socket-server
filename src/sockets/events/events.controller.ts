105f07873909691dc4a291601a1773b1b7ab9202
Yannick Simard <yannick.simard@unity3d.com>
2022-10-25 17:14:47 -0400

    Emit events coming from forma cloud

---
+++ src/sockets/events/events.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Logger, Param, Post } from "@nestjs/common";
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { UpdateEvent } from "./update-event";
+
+@Controller([
+    'sessions/:sessionId/:type',
+])
+export class EventsController {
+
+    private readonly logger = new Logger(EventsController.name);
+
+    constructor(private eventEmitter: EventEmitter2) { }
+
+    @Post()
+    async sessionDataUpdated(
+        @Param('sessionId') sessionId: string,
+        @Param('type') type: string,
+        @Body() body: any,
+    ): Promise<any> {
+        this.logger.debug(`Emitting ${type} ${sessionId}`);
+        await this.eventEmitter.emit('update', new UpdateEvent(type, sessionId));
+    }
+}
