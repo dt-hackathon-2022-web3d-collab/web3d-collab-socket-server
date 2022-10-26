cf9d6ff1d9ddf4cae13ea9538074e2c7720fb74a
Yannick Simard <yannick.simard@unity3d.com>
2022-10-25 20:47:18 -0400

    Added redis backend for cache config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { CacheModule, Module } from '@nestjs/common';
 import { SocketsModule } from './sockets/sockets.module';
 import { ConfigModule } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
+import type { ClientOpts } from 'redis';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), CacheModule.register({isGlobal: true}),SocketsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), CacheModule.register<ClientOpts>({
+    isGlobal: true,
+    store: redisStore,
+
+    host: 'localhost',
+    port: 6379,
+  }),SocketsModule],
   controllers: [],
   providers: [],
 })
